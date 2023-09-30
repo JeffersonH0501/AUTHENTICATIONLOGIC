c25547d66b9e9d702985c0dbf66b21226d27af73
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-09-29 23:57:31 -0500

    cambios

--- subscriber.py
+++ subscriber.py
@@ -29,7 +29,7 @@ for topic in topics:
 
 print('> Esperando autenticacion.')
 
-def callback(ch, method, body):
+def callback(ch, method, properties, body):
     try:
         usuario, clave = body.decode('utf-8').split()
         resultado = verificar_usuario(usuario, clave)
