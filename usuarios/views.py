6761c966ce4384dab2b406e02005fad3a4b1e960
JeffersonH0501 <ja.hernandezg1@uniandes.edu.co>
2023-11-16 10:59:19 -0500

    cambios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,36 +1,35 @@
+import jwt
+from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Usuario
 
 def verificar_usuario(documento, clave):
+
     try:
         usuario = Usuario.objects.get(documento=documento, clave=clave)
-        respuesta = "valido"
-        tipo = usuario.tipo
     except Usuario.DoesNotExist:
-        respuesta = "invalido"
-        tipo = ""
+        usuario = None
 
-    return respuesta, tipo
-
-def agregar_usuario(documento, clave, tipo):
-    nuevo_usuario = Usuario(documento=documento, clave=clave, tipo=tipo)
-    nuevo_usuario.save()
-    print("> documento: "+ documento + ", clave: " + clave + ", tipo: " + tipo + ". Agregado con exito")
+    return usuario
 
 class AutenticacionAPI(APIView):
 
     def post(self, request):
+        
+        documento = request.data.get("documento")
+        clave = request.data.get("clave")
 
-        documento = request.data.get('documento')
-        clave = request.data.get('clave')
+        usuario = verificar_usuario(documento, clave)
 
-        respuesta, tipo = verificar_usuario(documento, clave)
+        if usuario is not None:
 
-        respuesta_post = {'respuesta': respuesta, 'tipo': tipo}
+            token_payload = {"documento": usuario.documento, "tipo": usuario.tipo}
+            token = jwt.encode(token_payload, settings.SECRET_KEY, algorithm="HS256")
+
+            respuesta_post = {"respuesta": "valido", "token": token}
+        else:
+            respuesta_post = {"respuesta": "invalido"}
 
         return Response(respuesta_post, status=status.HTTP_200_OK)
-
-
-
